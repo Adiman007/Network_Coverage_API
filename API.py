d1f17a2b8d090d319c1c80c14ab37d05420d0ada
Adiman007 <adiman.bonnefoy@gmail.com>
2024-10-28 17:36:17 +0100

    - added a new route with fast time but accuraccy - modified closest_locations methods to return X closest locations based on range of 4g antenna - added the address to the api payload - added comments

--- API.py
+++ API.py
@@ -5,16 +5,21 @@ from geopy.distance import geodesic
 
 app = FastAPI()
 
-def three_closest_location_distance_formula(coord, data):
+# get the closest locations to a given coordinate using the distance formula
+def closest_locations_distance_formula(coord, data):
     data['distance'] = ((data['lat'] - coord[0])**2 + (data['long'] - coord[1])**2)**0.5
-    three_closest = data.nsmallest(3, 'distance')
-    return three_closest
+    closest_locations = data[data['distance'] < 0.008]
+    closest_locations = closest_locations.sort_values(by='distance', ascending=True)
+    return closest_locations
 
-def three_closest_location_geodesic(coord, data):
+# get the closest locations to a given coordinate using the Haversine formula
+def closest_locations_geodesic(coord, data):
     data['distance'] = data.apply(lambda x: geodesic((x['lat'], x['long']), coord).kilometers, axis = 1)
-    three_closest = data.nsmallest(3, 'distance')
-    return three_closest
+    closest_locations = data[data['distance'] < 1.5]
+    closest_locations = closest_locations.sort_values(by='distance', ascending=True)
+    return closest_locations
 
+# get the coordinates of an address using the api-adresse.data.gouv.fr API
 def address_to_coordinates(q):
     response = requests.get(f'https://api-adresse.data.gouv.fr/search/?q={q}')
     if response.status_code == 200:
@@ -22,21 +27,23 @@ def address_to_coordinates(q):
         if data['features']:
             coords = data['features'][0]['geometry']['coordinates']
             return coords[1], coords[0]
-        
+
+# import the data from a csv file (here clear_data.csv)        
 def import_data(file):
     data = pd.read_csv(file, sep = ";")
     return data
 
+#root
 @app.get("/")
 def read_root():
     return {"Hello": "World"} 
 
+#uses geodesic to calculate the distance with accuracy but way slower
 @app.get("/network/geodesic/{q}")
 def read_test(q: str):
     coord = address_to_coordinates(q)
     data = import_data('clear_data.csv')
-    locs = three_closest_location_geodesic(coord, data)
-
+    locs = closest_locations_geodesic(coord, data)
     networks = {}
     for index, row in locs.iterrows():
         if row["Nom_Operateur"] not in networks:
@@ -48,18 +55,20 @@ def read_test(q: str):
 
     result = {
         "Networks": networks,
-        "coords": {"lat": coord[0], "lon": coord[1]}
+        "coords": {"lat": coord[0], "long": coord[1]},
+        "address" : q
     }
+    
     
     return result
 
+#uses distance formula to filter out the closest locations very quickly but with less accuracy
 @app.get("/network/distance/{q}")
 def read_test(q: str):
 
     coord = address_to_coordinates(q)
     data = import_data('clear_data.csv')
-    locs = three_closest_location_distance_formula(coord, data)
-
+    locs = closest_locations_distance_formula(coord, data)
     networks = {}
     for index, row in locs.iterrows():
         if row["Nom_Operateur"] not in networks:
@@ -71,7 +80,32 @@ def read_test(q: str):
 
     result = {
         "Networks": networks,
-        "coords": {"lat": coord[0], "lon": coord[1]}
+        "coords": {"lat": coord[0], "long": coord[1]},
+        "address" : q
+    }
+    
+    return result
+
+#uses distance formula to filter out the closest locations and then geodesic to calculate the distance with accuracy
+@app.get("/network/{q}")
+def read_test(q: str):
+    coord = address_to_coordinates(q)
+    data = import_data('clear_data.csv')
+    close_locs = closest_locations_distance_formula(coord, data)
+    locs = closest_locations_geodesic(coord, close_locs)
+    networks = {}
+    for index, row in locs.iterrows():
+        if row["Nom_Operateur"] not in networks:
+            networks[row["Nom_Operateur"]] = {
+                "2G": row['2G'] != 0,
+                "3G": row['3G'] != 0,
+                "4G": row['4G'] != 0
+            }
+
+    result = {
+        "Networks": networks,
+        "coords": {"lat": coord[0], "long": coord[1]},
+        "address" : q
     }
     
     return result
